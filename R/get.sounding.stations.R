5b75d7770c686026d9735d69a4199cafc3d01f67
rich-iannone <riannone@me.com>
2013-12-29 23:39:10 -0800

    Include regex pattern string for extracting station data

--- R/get.sounding.stations.R
+++ R/get.sounding.stations.R
@@ -5,5 +5,9 @@ get.sounding.stations <- function(){
   
   # Obtain the HTML source from a URI containing a query
   URI <- getURL("http://www.esrl.noaa.gov/raobs/intl/GetRaobs.cgi?shour=All+Times&ltype=All+Levels&wunits=Tenths+of+Meters%2FSecond&bdate=1990010100&edate=2013122523&access=All+Sites&view=YES&osort=Station+Series+Sort&oformat=FSL+format+%28ASCII+text%29")
+  
+  # Create a 'pattern' string object containing the regex pattern for extracting sounding data strings
+  # from the URI
+  pattern <- "<OPTION> [0-9A-Z]*[ ]*[0-9]* [0-9]{5} [0-9/.-]* [0-9/.-]* [0-9-]{5,6}  [.]*  [0-9A-Z]{2} [0-9A-Z]{2}"
   # Close function
 }
