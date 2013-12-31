33fe3a9e157073d7a8395aa3405e4c69f587929a
rich-iannone <riannone@me.com>
2013-12-30 23:48:59 -0800

    Change name of ‘sounding_data’ object

--- R/process.sounding.data.R
+++ R/process.sounding.data.R
@@ -8,18 +8,18 @@ process.sounding.stations <- function(sounding_data_vector){
   
   
   # Verify that a new record is identified in the current line
-  is_254 <- str_detect(sounding_data_2[1], "^    254")
-  is_1 <- str_detect(sounding_data_2[1], "^      1")
-  is_2 <- str_detect(sounding_data_2[1], "^      2")
-  is_3 <- str_detect(sounding_data_2[1], "^      3")
-  is_4_9 <- str_detect(sounding_data_2[1], "^      [4-9]")
+  is_254 <- str_detect(sounding_data[1], "^    254")
+  is_1 <- str_detect(sounding_data[1], "^      1")
+  is_2 <- str_detect(sounding_data[1], "^      2")
+  is_3 <- str_detect(sounding_data[1], "^      3")
+  is_4_9 <- str_detect(sounding_data[1], "^      [4-9]")
   
-  header <- read.table(textConnection(sounding_data_2[1]),
+  header <- read.table(textConnection(sounding_data[1]),
                        stringsAsFactors = FALSE)
   
   # If line is a header line, read it as such
   if (is_254 == TRUE) {
-    header <- read.table(textConnection(sounding_data_2[1]),
+    header <- read.table(textConnection(sounding_data[1]),
                          stringsAsFactors = FALSE)
     colnames(header) <- c("linetyp", "hour", "day", "month", "year")
   }
