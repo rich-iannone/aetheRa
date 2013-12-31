c154accf9c087b5535183435d3dd6879d6e6d903
rich-iannone <riannone@me.com>
2013-12-30 17:13:19 -0800

    Define arguments/defaults for ‘get.sounding.data'

--- R/get.sounding.data.R
+++ R/get.sounding.data.R
@@ -1,2 +1,13 @@
+get.sounding.data <- function(start_date,
+                              end_date,
+                              hour_type = "all",
+                              level_type = "all",
+                              wind_units = "tenths_ms",
+                              station_number = NULL,
+                              station_wban_wmo = NULL,
+                              output_file_path = "working",
+                              output_file_name = "FSL-Sounding.txt",
+                              details_in_file_name = TRUE
+){
 }
 
