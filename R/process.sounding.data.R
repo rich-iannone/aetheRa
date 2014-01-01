d64312dda6d43443bddb6fc76ec1ae491cc94f59
rich-iannone <riannone@me.com>
2013-12-31 19:37:35 -0800

    Push existing code to remote

--- R/process.sounding.data.R
+++ R/process.sounding.data.R
@@ -5,19 +5,93 @@ process.sounding.stations <- function(sounding_data_vector){
   require(stringr)
   
   # Determine number of soundings in file, necessary for initialization of list object
+  # Check for equal numbers of header lines (254, 1, 2, and 3)
   soundings_254 <- length(grep(pattern = "^    254", x = sounding_data))
   soundings_1 <- length(grep(pattern = "^      1", x = sounding_data))
   soundings_2 <- length(grep(pattern = "^      2", x = sounding_data))
   soundings_3 <- length(grep(pattern = "^      3", x = sounding_data))
   
+  # The condition of full headers (having line types 254, 1, 2, and 3) is verified
+  # by seeing if the counts are all equal
   full_headers <- ifelse(soundings_254 == soundings_1 &
                            soundings_1 == soundings_2 &
                            soundings_2 == soundings_3, TRUE, FALSE)
   
-  if (full_headers == TRUE) soundings <- soundings_254
+  correct_first_line <- ifelse(1 %in% grep(pattern = "^    254",
+                                           x = sounding_data),
+                               TRUE, FALSE)
   
-  # This function parses the 'sounding_data' object and compose a list of data frames
+  # If the full headers condition passes and the first line contains the first header line,
+  # the number of soundings is passed to the 'soundings' object
+  if (full_headers == TRUE & correct_first_line == TRUE) soundings <- soundings_254
   
+  # Create list of soundings
+  sounding_list <- vector("list", soundings)
+  
+  # Each list element in sounding_list will have two data frames:
+  # (1) header information (e.g., time, sounding launch coordinates, etc.), and
+  # (2) sounding data (e.g., pressure, height, temperature, DPT, WD, WS, and
+  # sounding type)
+  
+  # Set iterator index (i) to 1; this corresponds the line number in 'sounding_data'
+  # vector list
+  i <- 1
+  
+  # Set iterator index (list) to 1; this corresponds to the sounding number (and the
+  # list number)
+  list <- 1
+  
+  # Use a while loop to cycle through the 'sounding_data' object and extract element
+  while ( i < length(sounding_data)) {
+    header_254 <- read.table(textConnection(sounding_data[i]),
+                         stringsAsFactors = FALSE)
+    colnames(header_254) <- c("lintyp_254", "hour", "day", "month", "year")
+    
+    header_1 <- read.table(textConnection(sounding_data[i + 1]),
+               stringsAsFactors = FALSE)
+    colnames(header_1) <- c("lintyp_1", "wban", "wmo", "lat", "lon", "elev", "rtime")
+    
+    header_2 <- read.table(textConnection(sounding_data[i + 2]),
+                           stringsAsFactors = FALSE)
+    colnames(header_2) <- c("lintyp_2", "hydro", "mxwd", "tropl", "lines", "tindex", "source")
+    
+    header_3 <- read.table(textConnection(sounding_data[i + 3]),
+                           stringsAsFactors = FALSE)
+    colnames(header_3) <- c("lintyp_3", "staid", "sonde", "wsunits")
+    
+    header <- cbind(header_254, header_1, header_2, header_3)
+    header$lintyp_254 <- NULL
+    header$lintyp_1 <- NULL
+    header$lintyp_2 <- NULL
+    header$lintyp_3 <- NULL
+    
+    sounding_list[[1]][[1]] <- header
+  
+    j <- 5
+    for (j in (i + 4):(i + header$lines - 1)) {
+      # Initialize the data frame for the sounding data
+      if (j == (i + 4) mat <- as.data.frame(mat.or.vec(nr = header$lines - 4, nc = 7))
+      mat[1,] <- read.table(textConnection(sounding_data[j]),
+                             stringsAsFactors = FALSE)
+      
+    
+          
+          
+    }
+    
+    
+    sounding_list[[1]][[2]] <- df
+  
+  
+  
+  # Close while loop
+  }
+  
+  
+
+  
+  # 38 lines stated
+  # next 254 line is at 39
   
   # Verify that a new record is identified in the current line
   is_254 <- str_detect(sounding_data[1], "^    254")
