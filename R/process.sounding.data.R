ee67e167fd52fcc0863013fbe7245bed6c782667
rich-iannone <riannone@me.com>
2014-01-01 23:50:46 -0800

    Add incrementer for ‘list_item’ object

--- R/process.sounding.data.R
+++ R/process.sounding.data.R
@@ -68,6 +68,8 @@ process.sounding.stations <- function(sounding_data_vector){
     header$lintyp_2 <- NULL
     header$lintyp_3 <- NULL
     
+    # Increment the 'list_item' vector, corresponding to the sounding number
+    list_item <- list_item + 1
     for (j in (i + 4):(i + header$lines - 1)) {
       # Initialize the data frame for the sounding data
       if (j == (i + 4) mat <- as.data.frame(mat.or.vec(nr = header$lines - 4, nc = 7))
