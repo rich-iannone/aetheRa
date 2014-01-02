99d614dc4ea398649e75c34147b6f6a7e25f530a
rich-iannone <riannone@me.com>
2014-01-02 15:04:54 -0800

    Add ‘require’ statement for ‘lubridate’ package

--- R/export.for.CALMET.R
+++ R/export.for.CALMET.R
@@ -8,6 +8,7 @@ export.for.CALMET <- function(sounding_list = sounding_list,
   # Include require statements
   require(RCurl)
   require(stringr)
+  require(lubridate)
   
   # Check for existance of 'sounding_list' object, created by the
   # 'process.sounding.data' function; if the object doesn't exist, stop the function
