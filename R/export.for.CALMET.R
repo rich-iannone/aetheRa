8a5967d1f5fced6d23199d528ba902ec85830f5a
rich-iannone <riannone@me.com>
2014-01-02 14:27:49 -0800

    Check for existance of ‘sounding_list’ object

--- R/export.for.CALMET.R
+++ R/export.for.CALMET.R
@@ -4,5 +4,14 @@ export.for.CALMET <- function(sounding_list = sounding_list){
   require(RCurl)
   require(stringr)
   
+  # Check for existance of 'sounding_list' object, created by the
+  # 'process.sounding.data' function; if the object doesn't exist, stop the function
+  # with a message
+  if (!exists("sounding_data")) {
+    stop("Processed sounding data is not available. Use the 'process.sounding.data' function")
+  }
+  
+  
+  
   # Close the function
 }
