433a65b4877428b48e883cfc1c85d6ceb3a44c93
rich-iannone <riannone@me.com>
2014-01-01 23:48:46 -0800

    Remove older code from script

--- R/process.sounding.data.R
+++ R/process.sounding.data.R
@@ -80,34 +80,6 @@ process.sounding.stations <- function(sounding_data_vector){
     }
     
     
-    sounding_list[[1]][[2]] <- df
-  
-  
-  
-  # Close while loop
-  }
-  
-  
-
-  
-  # 38 lines stated
-  # next 254 line is at 39
-  
-  # Verify that a new record is identified in the current line
-  is_254 <- str_detect(sounding_data[1], "^    254")
-  is_1 <- str_detect(sounding_data[1], "^      1")
-  is_2 <- str_detect(sounding_data[1], "^      2")
-  is_3 <- str_detect(sounding_data[1], "^      3")
-  is_4_9 <- str_detect(sounding_data[1], "^      [4-9]")
-  
-  header <- read.table(textConnection(sounding_data[1]),
-                       stringsAsFactors = FALSE)
-  
-  # If line is a header line, read it as such
-  if (is_254 == TRUE) {
-    header <- read.table(textConnection(sounding_data[1]),
-                         stringsAsFactors = FALSE)
-    colnames(header) <- c("linetyp", "hour", "day", "month", "year")
   }
   
 }
