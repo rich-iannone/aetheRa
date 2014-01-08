bd4e94f6992232553240e9ddda5bf35bf883c285
rich-iannone <riannone@me.com>
2014-01-08 10:38:30 -0800

    Clean up construction of ‘URI’ object by splitting

--- R/get.sounding.stations.R
+++ R/get.sounding.stations.R
@@ -4,7 +4,11 @@ get.sounding.stations <- function(){
   require(stringr)
   
   # Obtain the HTML source from a URI containing a query
-  URI <- getURL("http://www.esrl.noaa.gov/raobs/intl/GetRaobs.cgi?shour=All+Times&ltype=All+Levels&wunits=Tenths+of+Meters%2FSecond&bdate=1990010100&edate=2013122523&access=All+Sites&view=YES&osort=Station+Series+Sort&oformat=FSL+format+%28ASCII+text%29")
+  URI <- getURL(paste("http://www.esrl.noaa.gov/raobs/intl/GetRaobs.cgi?",
+                      "shour=All+Times&ltype=All+Levels&wunits=Tenths+of+Meters%2FSecond",
+                      "&bdate=1990010100&edate=2013122523&access=All+Sites&view=YES&",
+                      "osort=Station+Series+Sort&oformat=FSL+format+%28ASCII+text%29",
+                      sep = ''))
   
   # Create a 'pattern' string object containing the regex pattern for extracting sounding data strings
   # from the URI
