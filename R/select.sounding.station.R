08a6ee828fd328706690b80569041feb95547e8f
rich-iannone <riannone@me.com>
2014-01-09 00:58:01 -0800

    Search data frame by either of init, wmo, or wban

--- R/select.sounding.station.R
+++ R/select.sounding.station.R
@@ -41,4 +41,47 @@ select.sounding.station <- function(df_soundings = df_soundings,
   #
   ####
   
+  #####
+  # Search the data frame using the search parameters
+  #####
+  
+  # Check that only one of the search parameters of 'search_init', 'search_wban',
+  # or 'search_wmo' are used; otherwise stop function and advise to only one of these
+  if (!is.null(search_init) & !is.null(search_wban)) {
+    stop("Only use one of 'search_init', 'search_wban', or 'search_wmo' search parameters")
+  }
+  
+  if (!is.null(search_wban) & !is.null(search_wmo)) {
+    stop("Only use one of 'search_init', 'search_wban', or 'search_wmo' search parameters")
+  }
+  
+  if (!is.null(search_wmo) & !is.null(search_init)) {
+    stop("Only use one of 'search_init', 'search_wban', or 'search_wmo' search parameters")
+  }
+  
+  # If a search by 'init' is requested, subset the stations data frame
+  if (!is.null(search_init)) df_soundings.subset <- subset(df_soundings,
+                                                           df_soundings$init == search_init)
+  if (!is.null(search_init) &
+        exists("df_soundings.subset") & 
+        nrow(df_soundings.subset) == 1) target_station <- df_soundings.subset
+  if (exists("target_station")) {
+    assign("target_station", target_station, envir = .GlobalEnv)
+    return(paste("The following station was identified and set as the target station: wmo ",
+                 target_station$wmo, ", wban ", target_station$wban, " (",
+                 target_station$station_name, ")", sep = ''))
+  }
+  
+  # If a search by 'wban' is requested, subset the stations data frame
+  if (!is.null(search_wban)) df_soundings.subset <- subset(df_soundings,
+                                                           df_soundings$wban == search_wban)
+  if (!is.null(search_wban) &
+        exists("df_soundings.subset") & 
+        nrow(df_soundings.subset) == 1) target_station <- df_soundings.subset
+  if (exists("target_station")) {
+    assign("target_station", target_station, envir = .GlobalEnv)
+    return(paste("The following station was identified and set as the target station: wmo ",
+               target_station$wmo, ", wban ", target_station$wban, " (",
+               target_station$station_name, ")", sep = ''))
+  }
 }
