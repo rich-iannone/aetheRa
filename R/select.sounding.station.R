6f9502fd8ec970a6757313eb240311074f7308b0
rich-iannone <riannone@me.com>
2014-01-08 23:40:02 -0800

    Initial commit of function for searching/selecting a station

---
+++ R/select.sounding.station.R
@@ -0,0 +1,20 @@
+select.sounding.station <- function(df_soundings = df_soundings,
+                                    id_by_wban_wmo = NULL,
+                                    search_station_name = NULL,
+                                    search_init = NULL,
+                                    search_wban = NULL,
+                                    search_wmo = NULL,
+                                    search_wban_wmo = NULL,
+                                    search_prov_state = NULL,
+                                    search_country = NULL,
+                                    lower_lat = NULL,
+                                    upper_lat = NULL,
+                                    lower_long = NULL,
+                                    upper_long = NULL,
+                                    lower_elev = NULL,
+                                    upper_elev = NULL
+){
+
+  ## stuff here
+  
+}
