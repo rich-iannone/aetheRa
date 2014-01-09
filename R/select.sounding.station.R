f6c1eb7025943d19cd6aca0bda0c116211cd91ee
rich-iannone <riannone@me.com>
2014-01-09 11:37:55 -0800

    Add search routines for searching by init and wmo

--- R/select.sounding.station.R
+++ R/select.sounding.station.R
@@ -58,24 +58,63 @@ select.sounding.station <- function(id_by_wban_wmo = NULL,
     stop("Only use one of 'search_init', 'search_wban', or 'search_wmo' search parameters")
   }
   
-#   # If a search by 'init' is requested, subset the stations data frame
-#   if (!is.null(search_init)) df_soundings.subset <- subset(df_soundings,
-#                                                            df_soundings$init == search_init)
-#   if (!is.null(search_init) &
-#         exists("df_soundings.subset")) {
-#         ifelse(nrow(df_soundings.subset) == 1,
-#                assign("target_station", df_soundings.subset, envir = .GlobalEnv), NULL)
-#   
-#   if (exists("target_station")) {
-#     return(paste("The following station was identified and set as the target station: wmo ",
-#                  target_station$wmo, ", wban ", target_station$wban, " (",
-#                  target_station$station_name, ")", sep = ''))
-#   }
   
+  #####
+  # If a search by 'init' is requested, subset the stations data frame
+  ####
+  
+  # Initial subset
+  if (!is.null(search_init)) df_soundings.subset <- subset(df_soundings,
+                                                           df_soundings$init == search_init)
+  
+  # If a subset was generated and is of zero length, return notification that
+  # no stations were found
+  if (!is.null(search_init) &
+        exists("df_soundings.subset")) {
+    if (nrow(df_soundings.subset) == 0) {
+      return(paste("No stations were identified from this search"))
+    }
+  }
+  
+  # If a subset was generated and is of single length, then a match has occurred
+  # Need to assign the subset as 'target_station' in the global environment and
+  # return a notification that a match was found
+  if (!is.null(search_init) &
+        exists("df_soundings.subset")) {
+    if (nrow(df_soundings.subset) == 1) {
+      assign("target_station", df_soundings.subset, envir = .GlobalEnv)
+      return(paste("The following station was identified and set ",
+                   "as the target station: wmo ",
+                   target_station$wmo, ", wban ", target_station$wban,
+                   " (", target_station$station_name, ")", sep = ''))
+    }
+  }
+  
+  # If a subset was generated that contains >100 records, return a notification
+  # stating the number of stations found (but don't return a df object)
+  # If the generated subset contains 2-100 records, return 'df_soundings.subset' 
+  if (!is.null(search_init) &
+        exists("df_soundings.subset")) {
+    if (nrow(df_soundings.subset) > 100) {
+      return(paste("A total of ", nrow(df_soundings.subset),
+                   " stations were identified from this search",
+                   sep = ''))
+    }
+    if (nrow(df_soundings.subset) > 1 & nrow(df_soundings.subset) <= 100) {
+      return(df_soundings.subset)
+    }
+  }
+  
+  #####
   # If a search by 'wban' is requested, subset the stations data frame
+  ####
+  
+  # Initial subset
   if (!is.null(search_wban)) df_soundings.subset <- subset(df_soundings,
                                                            df_soundings$wban == search_wban)
   
+  # If a subset was generated and is of zero length, return notification that
+  # no stations were found
   if (!is.null(search_wban) &
         exists("df_soundings.subset")) {
     if (nrow(df_soundings.subset) == 0) {
@@ -83,19 +122,23 @@ select.sounding.station <- function(id_by_wban_wmo = NULL,
     }
   }
   
+  # If a subset was generated and is of single length, then a match has occurred
+  # Need to assign the subset as 'target_station' in the global environment and
+  # return a notification that a match was found
   if (!is.null(search_wban) &
         exists("df_soundings.subset")) {
         if (nrow(df_soundings.subset) == 1) {
           assign("target_station", df_soundings.subset, envir = .GlobalEnv)
+          return(paste("The following station was identified and set ",
+                       "as the target station: wmo ",
+                       target_station$wmo, ", wban ", target_station$wban,
+                       " (", target_station$station_name, ")", sep = ''))
         }
   }
   
-  if (exists("target_station")) {
-    return(paste("The following station was identified and set as the target station: wmo ",
-                 target_station$wmo, ", wban ", target_station$wban, " (",
-                 target_station$station_name, ")", sep = ''))
-  }
-  
+  # If a subset was generated that contains >100 records, return a notification
+  # stating the number of stations found (but don't return a df object)
+  # If the generated subset contains 2-100 records, return 'df_soundings.subset' 
   if (!is.null(search_wban) &
         exists("df_soundings.subset")) {
     if (nrow(df_soundings.subset) > 100) {
@@ -108,5 +151,52 @@ select.sounding.station <- function(id_by_wban_wmo = NULL,
     }
   }
  
+  #####
+  # If a search by 'wmo' is requested, subset the stations data frame
+  ####
+  
+  # Initial subset
+  if (!is.null(search_wmo)) df_soundings.subset <- subset(df_soundings,
+                                                           df_soundings$wmo == search_wmo)
+  
+  # If a subset was generated and is of zero length, return notification that
+  # no stations were found
+  if (!is.null(search_wmo) &
+        exists("df_soundings.subset")) {
+    if (nrow(df_soundings.subset) == 0) {
+      return(paste("No stations were identified from this search"))
+    }
+  }
+  
+  # If a subset was generated and is of single length, then a match has occurred
+  # Need to assign the subset as 'target_station' in the global environment and
+  # return a notification that a match was found
+  if (!is.null(search_wmo) &
+        exists("df_soundings.subset")) {
+    if (nrow(df_soundings.subset) == 1) {
+      assign("target_station", df_soundings.subset, envir = .GlobalEnv)
+      return(paste("The following station was identified and set ",
+                   "as the target station: wmo ",
+                   target_station$wmo, ", wban ", target_station$wban,
+                   " (", target_station$station_name, ")", sep = ''))
+    }
+  }
+  
+  # If a subset was generated that contains >100 records, return a notification
+  # stating the number of stations found (but don't return a df object)
+  # If the generated subset contains 2-100 records, return 'df_soundings.subset' 
+  if (!is.null(search_wmo) &
+        exists("df_soundings.subset")) {
+    if (nrow(df_soundings.subset) > 100) {
+      return(paste("A total of ", nrow(df_soundings.subset),
+                   " stations were identified from this search",
+                   sep = ''))
+    }
+    if (nrow(df_soundings.subset) > 1 & nrow(df_soundings.subset) <= 100) {
+      return(df_soundings.subset)
+    }
+  }
+  
+  
   # Close the function
 }
