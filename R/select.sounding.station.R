ebdf83b107d064a97b88dbc20c1fabbca6c17ae5
rich-iannone <riannone@me.com>
2014-01-09 00:57:05 -0800

    Add test parameter input data

--- R/select.sounding.station.R
+++ R/select.sounding.station.R
@@ -15,5 +15,31 @@ select.sounding.station <- function(df_soundings = df_soundings,
 ){
 
   ## stuff here
+  # Test parameter inputs
+  #
+  #
+  
+#   #id_by_wban_wmo <- NULL
+#   
+#   #search_station_name = "the pas"
+#   
+#   search_init = NULL
+#   search_wban = "21603"
+#   search_wmo = NULL
+#   
+#   search_prov_state = NULL
+#   search_country = NULL
+#   
+#   lower_lat = NULL
+#   upper_lat = NULL
+#   lower_long = NULL
+#   upper_long = NULL
+#   
+#   lower_elev = NULL
+#   upper_elev = NULL
+  
+  #
+  #
+  ####
   
 }
